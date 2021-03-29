792de2fabdb6906fe8ad53f9ef23570228320e72
vkaytsanov <vkaytsanov@yahoo.com>
2021-03-29 18:26:52 +0300

    removed redundant switch statement in has_pending_signals

--- kernel/signal.c
+++ kernel/signal.c
@@ -128,24 +128,9 @@ static inline bool has_pending_signals(sigset_t *signal, sigset_t *blocked)
 	unsigned long ready;
 	long i;
 
-	switch (_NSIG_WORDS) {
-	default:
-		for (i = _NSIG_WORDS, ready = 0; --i >= 0 ;)
-			ready |= signal->sig[i] &~ blocked->sig[i];
-		break;
+	for (i = _NSIG_WORDS, ready = 0; --i >= 0 ;)
+		ready |= signal->sig[i] &~ blocked->sig[i];
 
-	case 4: ready  = signal->sig[3] &~ blocked->sig[3];
-		ready |= signal->sig[2] &~ blocked->sig[2];
-		ready |= signal->sig[1] &~ blocked->sig[1];
-		ready |= signal->sig[0] &~ blocked->sig[0];
-		break;
-
-	case 2: ready  = signal->sig[1] &~ blocked->sig[1];
-		ready |= signal->sig[0] &~ blocked->sig[0];
-		break;
-
-	case 1: ready  = signal->sig[0] &~ blocked->sig[0];
-	}
 	return ready !=	0;
 }
 
